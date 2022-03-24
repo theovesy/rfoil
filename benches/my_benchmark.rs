6c12c2b1b57234dac8f3a5d09d485497a6a5549e
theovesy <theovesy@hotmail.fr>
2022-03-24 20:21:26 +0100

    lib and benchmarks

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,9 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rfoil;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("rfoil", |b| b.iter(|| rfoil::run_sim()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
